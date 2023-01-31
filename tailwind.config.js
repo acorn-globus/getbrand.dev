30cbfa93a56276aee82f36554688b985dd3763b8
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-02-01 05:23:46 +0530

    a new gunmetal color added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
             colors: {
                 'woodsmoke': '#12171A',
                 'pastelblue': '#B2BECD',
+                'gunmetal': '#2A2E35',
                 'primary': '#3C82F6',
           }
       },
