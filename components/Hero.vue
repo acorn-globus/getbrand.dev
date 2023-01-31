eb478b625e55fcdf72e559d71df24b5cfe3290a9
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-01-31 21:50:58 +0530

    some changes in tailwind components and Hero section

---
+++ components/Hero.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-woodsmoke text-white py-28 px-5">
+    <div class="container mx-auto flex flex-col items-center text-center gap-8">
+      <h1 class="text-5xl md:text-6xl font-bold leading-normal">
+        Transform Your Online Presence
+      </h1>
+      <p class="text-base md:text-lg text-pastelblue">
+        One source of truth for all your brand assets and API for branded
+        experiences.
+      </p>
+      <div class="flex gap-4">
+        <button
+          class="
+            btn
+            bg-primary
+            text-white
+            hover:shadow-lg hover:shadow-primary/20 hover:-translate-y-0.5
+            transition
+            duration-300
+          "
+        >
+          Get API Key
+        </button>
+        <button
+          class="
+            btn
+            bg-transparent
+            border border-pastelblue
+            text-white
+            hover:shadow-lg hover:shadow-pastelblue/10 hover:-translate-y-0.5
+            transition
+            duration-300
+          "
+        >
+          Book Now
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
