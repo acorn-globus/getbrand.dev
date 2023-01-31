55bdc02e4c37c415661d80bf7a4cde3175d8f1da
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-01-31 17:34:29 +0530

    font & tailwind cofig file setup, and some assets(image)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Satoshi', 'sans-serif']
+            },
+            colors: {
+                'woodsmoke': '#12171A',
+                'pastelblue': '#B2BECD',
+                'primary': '#3C82F6',
+          }
+      },
+    },
+    plugins: [],
+  }
