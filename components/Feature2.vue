6bf01dde5d9ce19dc4b8ec29f42d20cb6197e0e3
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-02-01 05:26:43 +0530

    feature section with 3 feature component

---
+++ components/Feature2.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-woodsmoke py-16 px-5">
+    <div
+      class="container flex flex-col lg:flex-row items-center gap-14 mx-auto"
+    >
+      <img
+        src="~/assets/images/elevate-your-user-experience.png"
+        alt="Image demonstrating Elevate your User Experience"
+      />
+      <div class="flex flex-col gap-8 lg:order-first">
+        <div class="flex flex-col gap-2">
+          <h2 class="text-4xl md:text-5xl text-white">
+            Elevate Your User Experience
+          </h2>
+          <p class="text-base md:text-lg text-pastelblue">
+            Make a lasting impression on new users by tailoring your product to
+            their brand preferences, and help them reach that "aha" moment
+            faster.
+          </p>
+        </div>
+        <ul class="text-pastelblue text-sm md:text-base">
+          <li class="flex items-start mb-4 gap-2">
+            <img src="~/assets/images/tick.svg" alt="tick mark" />
+            Streamline the onboarding process with personalized brand
+            suggestions
+          </li>
+          <li class="flex items-start mb-4 gap-2">
+            <img src="~/assets/images/tick.svg" alt="tick mark" />
+            Design unique and branded templates to set your product apart
+          </li>
+          <li class="flex items-start mb-4 gap-2">
+            <img src="~/assets/images/tick.svg" alt="tick mark" />
+            Make it easy for users to find and identify their desired brand with
+            autocomplete feature
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ components/FeatureList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Feature1 />
+    <Feature2 />
+    <Feature3 />
+  </div>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {
+      feature: {
+        title: "Unleash the Power of Personalization",
+        desc: "Make your users feel like they're in the right place by automatically filling in logos, colors, fonts, and more for every brand",
+        imgName: "power-of-personalization.png",
+        imgAlt: "Image demonstrating Power of Powersonalization",
+        benefits: [
+          "Never settle for outdated logos again with our always-up-to-date. library.",
+          "Take personalization to the next level with banners, colors, and fonts.",
+          "Get instant access to brand names, descriptions, and links for a seamless user experience.",
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
