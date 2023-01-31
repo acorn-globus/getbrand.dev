82a143827473591c9999b7696bca792d0c1c0f8a
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-01-31 17:43:42 +0530

    footer

---
+++ components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer class="bg-woodsmoke py-16 text-pastelblue">
+    <div class="container flex flex-col items-center mx-auto gap-6">
+      <img
+        class="w-24"
+        src="~/assets/images/getbrand_logo.svg"
+        alt="GetBrand"
+      />
+      <div class="flex gap-6">
+        <div class="flex flex-col md:flex-row gap-6">
+          <a
+            href=""
+            class="hover:text-primary transition duration-300 ease-in-out"
+            >Why GetBrand?</a
+          >
+          <a
+            href=""
+            class="hover:text-primary transition duration-300 ease-in-out"
+            >Brand</a
+          >
+          <a
+            href=""
+            class="hover:text-primary transition duration-300 ease-in-out"
+            >Developers</a
+          >
+        </div>
+        <div class="flex flex-col md:flex-row gap-6">
+          <a
+            href=""
+            class="hover:text-primary transition duration-300 ease-in-out"
+            >Blog</a
+          >
+          <a
+            href=""
+            class="hover:text-primary transition duration-300 ease-in-out"
+            >Signin</a
+          >
+          <a
+            href=""
+            class="hover:text-primary transition duration-300 ease-in-out"
+            >Register</a
+          >
+        </div>
+      </div>
+      <p class="text-sm opacity-50">© 2023 GetBrand</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
